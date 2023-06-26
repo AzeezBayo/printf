2bd7cf8468fe5f9040bac356940bf764aba02e06
Tawanda Dausi <tawiedausi@gmail.com>
2023-06-26 14:02:18 +0000

    3 and 5

---
+++ string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_string - writes character c to stdout
+ * @s: the string to print
+ * Return: 1
+ */
+int print_string(va_list s)
+{
+	char *my_string;
+	int i = 0;
+
+	my_string = va_arg(s, char *);
+
+	if (my_string == NULL)
+		my_string = "(null)";
+	while (my_string[i])
+	{
+		_putchar(my_string[i]);
+		i++;
+	}
+	return (i);
+}
